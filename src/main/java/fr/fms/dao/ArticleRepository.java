514a726cd90432bdb2263df82c0c4c233adfd3ef
Saralune <sarahlef59@hotmail.fr>
2022-06-21 14:08:37 +0200

    add package fr.fms.web

--- src/main/java/fr/fms/dao/ArticleRepository.java
+++ src/main/java/fr/fms/dao/ArticleRepository.java
@@ -1,5 +1,7 @@
 package fr.fms.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import fr.fms.entities.Article;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article,Long> {
-
+	Page<Article> findByDescriptionContains(String description, Pageable pageable);
 }
