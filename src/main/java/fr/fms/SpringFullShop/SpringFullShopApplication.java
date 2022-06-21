274e712be99ba1d724108e3e98d72806e03708ad
DuffauJC <jeancharles.duffau@gmail.com>
2022-06-21 10:21:23 +0200

    initialisation

---
+++ src/main/java/fr/fms/SpringFullShop/SpringFullShopApplication.java
@@ -0,0 +1,13 @@
+package fr.fms.SpringFullShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/fms/SpringFullShop/SpringFullShopApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.SpringFullShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
