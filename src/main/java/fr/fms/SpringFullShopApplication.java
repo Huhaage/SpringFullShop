bad9e3fba2edaf3a28b47838597d3c5b4c04d2d0
DuffauJC <jeancharles.duffau@gmail.com>
2022-06-21 16:02:38 +0200

    articles ok

--- src/main/java/fr/fms/SpringFullShopApplication.java
+++ src/main/java/fr/fms/SpringFullShopApplication.java
@@ -27,18 +27,18 @@ public class SpringFullShopApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		Category smartphone = categoryRepository.save(new Category(null,"Smartphone",null));
-        Category tablet = categoryRepository.save(new Category(null,"Tablet",null)); 
-        Category pc = categoryRepository.save(new Category(null,"PC",null));
-		Category logicielCategory = categoryRepository.save(new Category(null,"logiciel",null));
+		// Category smartphone = categoryRepository.save(new Category(null,"Smartphone",null));
+        // Category tablet = categoryRepository.save(new Category(null,"Tablet",null)); 
+        // Category pc = categoryRepository.save(new Category(null,"PC",null));
+		// Category logicielCategory = categoryRepository.save(new Category(null,"logiciel",null));
 		
-//		articleRepository.save(new Article(null,"S8","SamSung",350,smartphone));
-//		articleRepository.save(new Article(null,"S10","SamSung",400,smartphone));
-//		articleRepository.save(new Article(null,"S11","SamSung",420,smartphone));
-//		articleRepository.save(new Article(null,"SE","iPhone",650,smartphone));
-//		articleRepository.save(new Article(null,"Redmi Go","Xiaomi",200,smartphone));
-//		articleRepository.save(new Article(null,"Mate X","Huawei",180,smartphone));
-//		articleRepository.save(new Article(null,"Redmi9A","Xiaomi",150,smartphone));
+		// articleRepository.save(new Article(null,"S8","SamSung",350,"unknown.png", smartphone));
+		// articleRepository.save(new Article(null,"S10","SamSung",400,"unknown.png",smartphone));
+		// articleRepository.save(new Article(null,"S11","SamSung",420,"unknown.png",smartphone));
+		// articleRepository.save(new Article(null,"SE","iPhone",650,"unknown.png",tablet));
+		// articleRepository.save(new Article(null,"Redmi Go","Xiaomi",200,"unknown.png",smartphone));
+		// articleRepository.save(new Article(null,"Mate X","Huawei",180,	"unknown.png",logicielCategory));
+		// articleRepository.save(new Article(null,"Redmi9A","Xiaomi",150,"unknown.png",pc));
 
 		
 		
