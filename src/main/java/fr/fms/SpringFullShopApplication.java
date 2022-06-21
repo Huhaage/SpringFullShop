2fa9d2355a67f240680487ac0cf6b98138a30941
Saralune <sarahlef59@hotmail.fr>
2022-06-21 14:59:17 +0200

    fix home mapping

--- src/main/java/fr/fms/SpringFullShopApplication.java
+++ src/main/java/fr/fms/SpringFullShopApplication.java
@@ -1,13 +1,28 @@
 package fr.fms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import fr.fms.dao.ArticleRepository;
+import fr.fms.dao.CategoryRepository;
+
 @SpringBootApplication
-public class SpringFullShopApplication {
+public class SpringFullShopApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringFullShopApplication.class, args);
 	}
 
+	@Autowired
+	private ArticleRepository articleRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+  
+	@Override
+	public void run(String... args) throws Exception {
+		
+	}
 }
