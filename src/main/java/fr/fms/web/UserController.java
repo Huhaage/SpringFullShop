e3ee55f320b4f167b17d28186f5088c736d1e36c
DuffauJC <jeancharles.duffau@gmail.com>
2022-06-23 08:46:32 +0200

    ajout login et controller

--- src/main/java/fr/fms/web/UserController.java
+++ src/main/java/fr/fms/web/UserController.java
@@ -4,6 +4,7 @@
 package fr.fms.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @author CHJCS
@@ -12,4 +13,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UserController {
 
+    @GetMapping("/login")
+    public String login() {
+        
+        return "login";
+    }
 }
