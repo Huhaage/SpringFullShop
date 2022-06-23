ed1485f5cf74149cdcc5e567eb71a162d945150d
chrislozzi <mounede7@gmail.com>
2022-06-23 13:36:40 +0200

    christopheCaddy4

--- src/main/java/fr/fms/web/CaddyControler.java
+++ src/main/java/fr/fms/web/CaddyControler.java
@@ -1,5 +1,6 @@
 package fr.fms.web;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,33 +8,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.fms.dao.ArticleRepository;
 import fr.fms.entities.Article;
 
 @Controller
 public class CaddyControler {
-	
-	  @Autowired ArticleRepository articleRepository;
-	  
-	  Map<Integer ,Article> caddy = null; 
-	  List<Article> ListCaddy = null;
-	  
-	  @GetMapping("/caddy") public String caddy(Model model) {
-	  
-	  model.addAttribute("caddy", caddy); 
-	  model.addAllAttributes(ListCaddy);
-	  
-	  return "caddy"; 
-	  }
-	  
-	  @GetMapping("/addToCart")
-		public String delete(Long id, int page, String keyword) {
 
-			articleRepository.deleteById(id);
+	@Autowired ArticleRepository articleRepository;
 
-			return "redirect:/index?page="+page+"&keyword="+keyword;
+	Map<Long ,Article> caddy = new HashMap<Long ,Article>(); 
+	//List<Article> ListCaddy = caddy.;
 
-		}
-	 
+	@GetMapping("/caddy") public String caddy(Model model) {	  
+		model.addAttribute("caddy", caddy); 
+		//model.addAllAttributes(ListCaddy);
+
+		return "caddy"; 
+	}
+
+	@GetMapping("/addToCaddy")
+	public String addToCaddy(Long id, Model model, @RequestParam(name="page", defaultValue = "0") int page,
+									  @RequestParam(name="keyword", defaultValue = "") String keyword) {
+		System.out.println("id :" + id);
+		
+		Article article = articleRepository.findById(id).get();
+		
+		caddy.put(article.getId(), article) ;
+		System.out.println(caddy.get(id).getBrand());
+		
+
+		return "redirect:/articles?page="+page+"&keyword="+keyword;
+
+
+	}
+
 }
