ed97b3fc3bbd429935b79197d5fca1a5681e670c
Saralune <sarahlef59@hotmail.fr>
2022-06-27 15:33:27 +0200

    add fk user_id in customer table

--- src/main/java/fr/fms/entities/User.java
+++ src/main/java/fr/fms/entities/User.java
@@ -1,9 +1,12 @@
 package fr.fms.entities;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
@@ -22,4 +25,7 @@ public class User {
 	
 	private String login;
 	private String password;
+	
+	@OneToMany(mappedBy="user")
+	private Collection<Customer> customers;
 }
