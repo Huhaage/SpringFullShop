50c9057449fadee245c9a6fe569db12d5dd5fdd5
DuffauJC <jeancharles.duffau@gmail.com>
2022-07-01 15:30:57 +0200

    retouche order.html

--- src/main/java/fr/fms/web/OrdersController.java
+++ src/main/java/fr/fms/web/OrdersController.java
@@ -4,23 +4,17 @@
 package fr.fms.web;
 
 import java.util.List;
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.fms.business.IBusinessImpl;
-import fr.fms.entities.Article;
-import fr.fms.entities.Category;
 import fr.fms.entities.Orders;
 import fr.fms.entities.OrdersItem;
-import fr.fms.entities.OrdersItemId;
 import fr.fms.services.GlobalException;
 
 /**
